24850b4e48ea760d7892c66ee61c52803efd5a1a
alexb997 <alex_alexandru_997@yahoo.ro>
2023-08-15 22:57:26 +0300

    database connection

---
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootTest
+@EnableSwagger2
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
